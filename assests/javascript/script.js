af6afa325d9217ec98267c51429ad538fb4a105a
Josh <jdogcrane@gmail.com>
2021-05-20 00:33:49 -0400

    added rounds that increment after each button press. Also allowing me for lots of flexibilty in the future

--- assests/javascript/script.js
+++ assests/javascript/script.js
@@ -1,39 +1,83 @@
-var timeLimit;
-var questions;
-var choices = {}
+var time;
 
+var choice1 = document.querySelector(".choice1");
+var choice2 = document.querySelector(".choice2");
+var choice3 = document.querySelector(".choice3");
+var choice4 = document.querySelector(".choice4");
+var question = document.querySelector(".question");
+var startgame = document.querySelector(".startgame");
+
+// hides start button & shows question and choices
 function startGame() {
-    document.getElementById("choice1").style.visibility = "visible";
-    document.getElementById("choice2").style.visibility = "visible";
-    document.getElementById("choice3").style.visibility = "visible";
-    document.getElementById("choice4").style.visibility = "visible";
-    document.getElementById("startgame").style.visibility = "hidden";
+    choice1.style.visibility = "visible";
+    choice2.style.visibility = "visible";
+    choice3.style.visibility = "visible";
+    choice4.style.visibility = "visible";
+    startgame.style.visibility = "hidden";
+    next();
+}
+// when the user picks 1 this function fires
+function pick1() {
+    next();
+}
+// when the user picks 2 this function fires
+function pick2() {
+    next();
+}
+// when the user picks 3 this function fires
+function pick3() {
+    next();
+}
+// when the user picks 4 this function fires
+function pick4() {
+    next();
 }
 
 
-function choice1() {
-    // change to question2 and new choices
-    console.log(choice1);
-    // this is always correct
-}
+// sets what question we are on
+var round = 0
 
-function choice2() {
-    //if pressed change to question2 and new choices
-    console.log(choice2);
-    // if pressed remove 10 from timer
-}
+function next() {
+    round++
+    console.log(round)
+    // question 1 & choices
+    if (round === 1) {
+        choice1.textContent = "a1";
+        choice2.textContent = "a2";
+        choice3.textContent = "a3";
+        choice4.textContent = "a4";
 
-function choice3() {
-    //if pressed change to question2 and new choices
-    console.log(choice3);
-    //if pressed change to question2 and new choices
-}
+        question.textContent = "Q1";
+    }
+    // question 2 & choices
+    if (round === 2) {
+        choice1.textContent = "b1";
+        choice2.textContent = "b2";
+        choice3.textContent = "b3";
+        choice4.textContent = "b4";
 
-function choice4() {
-    //if pressed change to question2 and new choices
-    console.log(choice4);
-    //if pressed change to question2 and new choices
-}
+        question.textContent = "Q2";
+    }
 
-// when I click start game = show choices and question
-// it then shows question screen
+    // question 3 & choices
+    if (round === 3) {
+        choice1.textContent = "c1";
+        choice2.textContent = "c2";
+        choice3.textContent = "c3";
+        choice4.textContent = "c4";
+
+        question.textContent = "Q3";
+    }
+
+    // question 4 & choices
+    if (round === 4) {
+        choice1.textContent = "d1";
+        choice1.textContent = "d2";
+        choice1.textContent = "d3";
+        choice1.textContent = "d4";
+
+        question.textContent = "Q4";
+    }
+    // returns the value of round
+    return round;
+}
