fef334071e821f8b8750d9e365cccf6bd9615816
Josh <jdogcrane@gmail.com>
2021-05-19 22:59:10 -0400

    got some basic logic in for choices. Set up highscore page. Made some styling changes.

---
+++ assests/javascript/script.js
@@ -0,0 +1,39 @@
+var timeLimit;
+var questions;
+var choices = {}
+
+function startGame() {
+    document.getElementById("choice1").style.visibility = "visible";
+    document.getElementById("choice2").style.visibility = "visible";
+    document.getElementById("choice3").style.visibility = "visible";
+    document.getElementById("choice4").style.visibility = "visible";
+    document.getElementById("startgame").style.visibility = "hidden";
+}
+
+
+function choice1() {
+    // change to question2 and new choices
+    console.log(choice1);
+    // this is always correct
+}
+
+function choice2() {
+    //if pressed change to question2 and new choices
+    console.log(choice2);
+    // if pressed remove 10 from timer
+}
+
+function choice3() {
+    //if pressed change to question2 and new choices
+    console.log(choice3);
+    //if pressed change to question2 and new choices
+}
+
+function choice4() {
+    //if pressed change to question2 and new choices
+    console.log(choice4);
+    //if pressed change to question2 and new choices
+}
+
+// when I click start game = show choices and question
+// it then shows question screen
